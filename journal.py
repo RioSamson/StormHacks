d57376195c44c2be23158a5e08f5dea6c53a2db0
Shawn <shawnhan20@gmail.com>
2022-02-19 00:08:16 -0800

    journal commit

---
+++ journal.py
@@ -0,0 +1,5 @@
+# The journal part of the app
+class Journal: 
+    def journal():
+        return "Welcome to the journal. Here, you can freely write about what you are feeling or what you are thinking about. Anything that comes to mind and your cat buddy will quietly listen to what you have to say."
+        
