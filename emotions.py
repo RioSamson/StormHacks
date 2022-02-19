8479b51add58d24a7189a63e359eaed95eced295
sla405 <steven_li_4@sfu.ca>
2022-02-19 13:09:02 -0800

    emotion/stress tracker

---
+++ emotions.py
@@ -0,0 +1,22 @@
+class Emotions:
+    emotions = ["angry", "stressed", "sad", "happy", "excited"]
+
+    def getEmotion(self):
+        while True:
+            emotion = input("How are you feeling today? (angry, stressed, sad, happy, excited) ")
+            if emotion in self.emotions:
+                break
+        scale = self.emotions.index(emotion)
+        if scale <= 2:
+            if scale == 1:
+                self.stressTracker()
+            print("Hmmm, that's not good. Would you like to talk about it in your journal?")
+            #direct user to journal
+        else:
+            print("Meoww! I'm so glad you are feeling", emotion, "today!")
+        
+    def stressTracker(self):
+        while True:
+            lvl = input("On a scale of 1-10, how stressed are you feeling? ")
+            if int(lvl) in range(1,11):
+                break
