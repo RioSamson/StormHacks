afe21e994c3d294bacf4fe1805e8a724cb0005b3
Shawn <shawnhan20@gmail.com>
2022-02-19 15:28:31 -0800

    water

--- waterIntake.py
+++ waterIntake.py
@@ -12,17 +12,28 @@ class WaterIntake:
             gender = input("Type 'male', 'female' or 'other': ")
             gender = gender.lower().strip()
             if (gender == "male"):
-                print("According to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 3 litres of water a day!")
+                print("According to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 12 cups of water a day!")
                 break
             elif (gender == "female"):
-                print("According to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 2.2 litres of water a day!")
+                print("According to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 9 cups of water a day!")
                 break
             elif (gender == "other"):
                 print("That's okay if you don't want to share your gender!")
-                print("For males, according to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 3 litres of water a day!")
-                print("For females, according to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 2.2 litres of water a day!")
+                print("According to The U.S. National Academies of Sciences, Engineering, and Medicine, they recommend around 9-12 cups of water a day!")
+                break
             else:
                 print("Oops! Sorry, please choose a valid option meow! /ᐠ .ᆺ. ᐟ\ﾉ")
+
+    def checkWater(self):
+        print("Let's check your hydration levels! ฅ^•ﻌ•^ฅ Meow!")
+        gender = input("Are you 'male', 'female', or prefer not to answer 'other'?")
+
+        gender = gender.lower().strip()
+        waterAmount = 0
+        waterAmount = input 
+
+
+
 
 water = WaterIntake()
 water.giveWaterInfo()
