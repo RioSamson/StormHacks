8562977029c3e2e7a3ebd39695493627dd15cbd2
Shawn <shawnhan20@gmail.com>
2022-02-19 13:59:54 -0800

    journal and water

--- journal.py
+++ journal.py
@@ -1,18 +1,18 @@
 # The journal part of the app
 from operator import length_hint
-from typing_extensions import Self
 from webbrowser import get
 
 class Journal: 
-    #numOfEntries = 0
-    def journal():
-        print("Welcome to the journal!")
+    numOfEntries = 0
+    def journal(self):
+        print("Meoww! Welcome to the journal!")
+        print("ฅ^•ﻌ•^ฅ")
         print("Here, you can freely write about what you are feeling or what you are thinking about.")
         print("Anything that comes to mind and your cat buddy will quietly listen to what you have to say.")
         userInput = input("Type in whatever is on your mind, or, if don't want to say anything, just type 'pass'.\n")
         userInput = userInput.lower()
         lengthOfInput = len(userInput)
-
+        self.numOfEntries
         if (userInput == "pass"):
             print("It's okay if you have nothing to share, you can always come back when you do, thank you!")
             return
@@ -22,5 +22,5 @@ class Journal:
         else:
             print("I appreciate your response. I understand it's not always easy to share this much.\nBut expressing your thoughts is always great!")
 
-
-Journal.journal()
+myJournal = Journal()
+myJournal.journal()
